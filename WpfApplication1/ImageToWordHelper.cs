50fc446c9e2b0cc0d809ff6b23ea2b55ab8ba643
hemanth09 <hemanthsaicharan@gmail.com>
2017-11-10 20:52:17 +0530

    Ready to deploy all the files to git

---
+++ WpfApplication1/ImageToWordHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using Microsoft.Office.Interop.Word;
+namespace WpfApplication1
+{
+    class ImageToWordHelper //:IDisposable
+    {
+        object miss = System.Reflection.Missing.Value;
+        object otrue = true;
+        object ofalse = false;
+         Microsoft.Office.Interop.Word.Application WordApp = new Microsoft.Office.Interop.Word.Application();
+        Document doc = null;
+        
+        public ImageToWordHelper(){
+            try
+            {
+                WordApp.Documents.Add();
+                WordApp.Visible = true;
+                doc = WordApp.ActiveDocument;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(" " + ex.Message);
+            }
+        }
+        public void PastetoWord(System.Drawing.Image image, System.Drawing.Imaging.ImageFormat format)
+        {
+            try
+            {
+                //Byte[] imageCapture = ToByteArray(bitmap, System.Drawing.Imaging.ImageFormat.Bmp);
+                
+                System.Windows.Clipboard.SetDataObject(image, true);
+                object start = 0;
+                object end = 0;
+                Microsoft.Office.Interop.Word.Range rng = doc.Range(ref start, ref end);
+                rng.Paste();
+                if (image != null )
+                {
+                    ((IDisposable)image).Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(" " + ex.Message);
+            }
+        }
+        public void SaveWordAndClose()
+        {
+            doc.Save();
+            System.Windows.MessageBox.Show("Word created ");
+            WordApp.Quit(ref otrue, ref miss, ref miss);
+            /*if (WordApp != null || doc!=null)
+            {
+                ((IDisposable)WordApp).Dispose();
+                ((IDisposable)doc).Dispose();
+            }*/
+            WordApp = null;
+            doc = null;
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public  partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public Bitmap imageCapture()
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                    Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            this.WindowStyle = WindowStyle.SingleBorderWindow;
+            this.WindowState = (WindowState)FormWindowState.Normal;
+            return bitmap;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CaptureButtonOverride();
+         }
+        ImageToWordHelper wordDocCreate = null;
+        private void CaptureButtonOverride()
+        {
+            try
+            {
+                minimizeWindow();
+            }
+            finally
+            {
+                Bitmap bitmapCapture = imageCapture();
+                if (wordDocCreate == null)
+                {
+                    wordDocCreate = new ImageToWordHelper();
+                    wordDocCreate.PastetoWord(bitmapCapture, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+                else
+                {
+                    wordDocCreate.PastetoWord(bitmapCapture, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+            }
+        }
+        private void SaveButtonOverride(ImageToWordHelper wordDocCreate)
+        {
+            wordDocCreate.SaveWordAndClose();
+            /*if (wordDocCreate != null)
+            {
+                ((IDisposable)wordDocCreate).Dispose();
+            }*/
+            
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveButtonOverride(wordDocCreate);
+            wordDocCreate = null;
+        }
+        public void minimizeWindow()
+        {
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = (WindowState)FormWindowState.Minimized;
+        }
+       
+    }
+    
+}
